58dfcb5741b263016c2d7ed3b383b7d13410f7f6
Anhagu <leejhsam330@gmail.com>
2022-12-08 11:53:33 +0900

    클릭 영역이 크게 잡히는 버그 수정

--- public/js/index.js
+++ public/js/index.js
@@ -1,21 +1,18 @@
 const headerUserNameElement = document.querySelector('.user');
-const userNameElement = document.querySelector('.user-name');
+const userNameElement = document.querySelector('#name');
 const localUserName = localStorage.getItem('userName');
 
 const setUserNameInnerHtml = (name) => {
   headerUserNameElement.innerHTML = `${name} <span>님</span>`;
-  userNameElement.innerHTML = `${name} <span>님</span>`;
+  userNameElement.textContent = name;
 };
 
 if (localUserName) {
   setUserNameInnerHtml(localUserName);
-  headerUserNameElement.innerHTML = `${localUserName} <span>님</span>`;
-  userNameElement.innerHTML = `${localUserName} <span>님</span>`;
 }
 
 userNameElement.onclick = () => {
   const userName = prompt('이름을 입력해 주세요');
-
   localStorage.setItem('userName', userName);
 
   setUserNameInnerHtml(userName);

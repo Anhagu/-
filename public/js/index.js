8d788fd2ac3691bef7a8854da15889368692c54b
Anhagu <leejhsam330@gmail.com>
2022-12-08 15:32:12 +0900

    사용자 학번, 이메일 받는 기능 추가 및 유효성 검사

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,5 @@
+const regEmail = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i;
+const regId = /^[0-9]{9}$/;
 const headerUserNameElement = document.querySelector('.user');
 const userNameElement = document.querySelector('#name');
 const localUserName = localStorage.getItem('userName');
@@ -17,3 +19,60 @@ userNameElement.onclick = () => {
 
   setUserNameInnerHtml(userName);
 };
+
+
+const userIdElement = document.querySelector('#id');
+const userEmailElement = document.querySelector('#email');
+const localUserId = localStorage.getItem('userId');
+const localUserEmail = localStorage.getItem('userEmail');
+
+
+// 학번 변경
+const setUserIdInnerHtml = (id) => {
+  userIdElement.textContent = id;
+};
+
+if (localUserId) {
+  setUserIdInnerHtml(localUserId);
+}
+
+userIdElement.onclick = () => {
+  const userId = prompt('학번을 입력해주세요');
+  if (userId == '') {
+    alert('학번을 입력해주세요.');
+    return false;
+  } else {
+    if (regId.exec(userId) === null) {
+      alert('9자리 숫자로 입력해주세요.');
+      return false;
+    }
+  }
+  localStorage.setItem('userId', userId);
+  setUserIdInnerHtml(userId);
+};
+
+// 이메일 변경
+const setUserEmailInnerHtml = (email) => {
+  userEmailElement.textContent = email;
+};
+
+if (localUserEmail) {
+  setUserEmailInnerHtml(localUserEmail);
+}
+
+userEmailElement.onclick = () => {
+  const userEmail = prompt('이메일을 입력해주세요');
+  if (userEmail == '') {
+    alert('이메일을 입력해주세요.');
+    return false;
+  } else {
+    if (regEmail.exec(userEmail) === null) {
+      alert('이메일 형식에 맞게 입력해주세요.');
+      return false;
+    }
+  }
+  localStorage.setItem('userEmail', userEmail);
+  setUserEmailInnerHtml(userEmail);
+};
+
+
